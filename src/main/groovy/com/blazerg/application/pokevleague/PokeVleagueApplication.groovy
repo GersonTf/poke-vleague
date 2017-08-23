4c064aadcb0d25fe5f7e15087c84e14e1af2bf6b
Blazerg <tnii.gerson@gmail.com>
2017-08-23 18:53:24 +0200

    First configuration files

---
+++ src/main/groovy/com/blazerg/application/pokevleague/PokeVleagueApplication.groovy
@@ -0,0 +1,12 @@
+package com.blazerg.application.pokevleague
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PokeVleagueApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PokeVleagueApplication, args
+	}
+}
