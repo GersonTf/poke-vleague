cb674811a6f03d2dc3640a670345d00372dd7fff
Blazerg <tnii.gerson@gmail.com>
2017-09-17 22:22:16 +0200

    getting a random pokemon (random fix)

--- src/main/groovy/com/blazerg/application/pokevleague/controller/PokemonController.groovy
+++ src/main/groovy/com/blazerg/application/pokevleague/controller/PokemonController.groovy
@@ -52,7 +52,7 @@ class PokemonController {
         final HttpEntity<String> entity = new HttpEntity<String>(headers)
 
         Random random = new Random()
-        def index = random.nextInt(791)
+        def index = random.nextInt((791 - 1) + 1) + 1
 
         ResponseEntity<String> response = restTemplate.exchange("http://pokeapi.co/api/v2/pokemon/$index", HttpMethod.GET, entity, String.class)
         response.getBody()
