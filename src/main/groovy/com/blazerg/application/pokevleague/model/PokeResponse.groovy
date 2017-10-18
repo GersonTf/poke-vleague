0a332da993c60e88ae6a8dceaa5734b796b69d7d
Gerson Cabrera <gerson.cabrera@externos.tinsa.com>
2017-10-18 13:29:58 +0200

    Form class rename to PokeResponse

---
+++ src/main/groovy/com/blazerg/application/pokevleague/model/PokeResponse.groovy
@@ -0,0 +1,12 @@
+package com.blazerg.application.pokevleague.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import groovy.transform.Canonical
+
+@Canonical
+@JsonIgnoreProperties(ignoreUnknown = true)
+class PokeResponse {
+
+    String name
+
+}
