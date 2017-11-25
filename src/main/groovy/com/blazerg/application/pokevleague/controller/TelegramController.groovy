fe2a695dff49540530a9ce0d259f894295f083f8
Blazerg <tnii.gerson@gmail.com>
2017-11-26 00:49:50 +0100

    temporal message

--- src/main/groovy/com/blazerg/application/pokevleague/controller/TelegramController.groovy
+++ src/main/groovy/com/blazerg/application/pokevleague/controller/TelegramController.groovy
@@ -4,12 +4,14 @@ import com.blazerg.application.pokevleague.model.Update
 import com.blazerg.application.pokevleague.service.MessageService
 import com.blazerg.application.pokevleague.service.PokemonService
 import groovy.util.logging.Log
+import io.swagger.annotations.Api
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
+@Api(value = 'TELEGRAM', description = 'The telegram shit')
 @Log
 @RequestMapping(value = "/listener")
 @RestController
@@ -29,8 +31,10 @@ class TelegramController {
         log.info("message received $inputMessage")
         log.info("$update")
 
-        if(inputMessage == "/getPoke"){
-             this.messageService.sendNotificationToTelegram(pokemonService.getExistingPoke().getName(), chatId)
+        if (inputMessage == "/getPoke") {
+            this.messageService.sendNotificationToTelegram("Ninjas sent to your coordinates", chatId)
+//            this.messageService.sendNotificationToTelegram(pokemonService.getExistingPoke().getName(), chatId)
+            log.info("Poke retrieved from the pokeApi")
         }
     }
 }
