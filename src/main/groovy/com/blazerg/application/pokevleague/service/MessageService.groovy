3056ac951e209f43d5f06f352e398b6743baabb7
Blazerg <tnii.gerson@gmail.com>
2017-11-25 03:24:53 +0100

    security settings

--- src/main/groovy/com/blazerg/application/pokevleague/service/MessageService.groovy
+++ src/main/groovy/com/blazerg/application/pokevleague/service/MessageService.groovy
@@ -24,9 +24,6 @@ class MessageService {
      * @param message to send
      */
     void sendNotificationToTelegram(String message) {
-        println environment.getProperty("PUEBA_123")
-
-        final String CHAT_ID = -279181328
 
         RestTemplate restTemplate = new RestTemplate()
         Date accessDate = new Date()
@@ -36,13 +33,13 @@ class MessageService {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED)
 
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>()
-        params.add("chat_id", CHAT_ID)
+        params.add("chat_id",  environment.getProperty("CHAT_ID"))
         params.add("text", finalMessage)
 
         HttpEntity<LinkedMultiValueMap<String, Object>> request = new HttpEntity<>(params, headers)
 
         restTemplate.postForEntity(
-                "https://api.telegram.org/bot496020190:AAHQSbdhA6mm02D7jpFDIK1Qjtq4lsYz2Js/sendMessage",
+                environment.getProperty("URL_TELEGRAM"),
                 request, String.class
         )
     }
