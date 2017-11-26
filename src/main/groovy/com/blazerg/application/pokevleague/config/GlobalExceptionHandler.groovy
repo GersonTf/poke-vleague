9a444641b846e3cf79594642f8d19854b22c84f5
Blazerg <tnii.gerson@gmail.com>
2017-11-26 15:52:37 +0100

    comment exception handler

--- src/main/groovy/com/blazerg/application/pokevleague/config/GlobalExceptionHandler.groovy
+++ src/main/groovy/com/blazerg/application/pokevleague/config/GlobalExceptionHandler.groovy
@@ -6,16 +6,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@Log
-@ControllerAdvice
-/**
- * this class catch the exception thrown if the bots sends a wrong or unknown command
- */
+//@Log
+//@ControllerAdvice
+///**
+// * this class catch the exception thrown if the bots sends a wrong or unknown command
+// */
 class GlobalExceptionHandler {
-    @ExceptionHandler(value = [MissingMethodException.class])
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST,
-            reason = "no method found for the input command message")
-    void handle(MissingMethodException e) {
-        log.info("no command found for the input message")
-    }
+//    @ExceptionHandler(value = [MissingMethodException.class])
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST,
+//            reason = "no method found for the input command message")
+//    void handle(MissingMethodException e) {
+//        log.info("no command found for the input message")
+//    }
 }
